7641603e999c83ea48e3f14e48233f51b402e960
sabrinaatha <sabrinaathashania@gmail.com>
2023-09-09 10:31:47 +0700

    masih progress ke arah deployment

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def show_main(request) :
     context = {
         'name': 'Sabrina Atha Shania',
         'class': 'PBP-A',
-        'application_name': 'Book Collections of Galaxy Library',
+        'application_name': 'Galaxy Library',
     }
     book1 = Books.objects.get_or_create(name='Harry Potter and the Philosopher\'s Stone', amount=10, description= 'The first book in the Harry Potter series was entitled Harry Potter and the Philosopher\'s Stone. This series opening novel is the part that introduces every important character in the Harry Potter story. The story in this first novel comes from the story of Harry Potter, who for 11 years has lived a miserable life with his cousin\'s family.')
     Books.objects.get_or_create(name='Harry Potter and the Chamber of Secrets', amount=35, description= 'The second book in the Harry Potter series is entitled Harry Potter and the Chamber of Secrets. This second book tells the story of Harry Potter and his friends when their school was in an uproar because of a mysterious terror. Harry and his friends then have to face threats from monsters that are in the Chamber of Secrets at Hogwarts.')
