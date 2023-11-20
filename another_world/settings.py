144a05b9d5513bc77f86dd07ceddb910f6bc816c
sabrinaatha <sabrinaathashania@gmail.com>
2023-11-21 00:55:56 +0700

    revert

--- another_world/settings.py
+++ another_world/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'authentication',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'another_world.urls'
